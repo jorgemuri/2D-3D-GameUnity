27fdc81b6d1b8547f880358730f403fca9d12112
jorgemuri <jmurilloc12@iescastelar.com>
2024-11-18 13:28:48 +0100

    Mejora de las cosas dadas en clase

--- murillojGame/Assets/Scripts/CameraController.cs
+++ murillojGame/Assets/Scripts/CameraController.cs
@@ -5,14 +5,11 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     public float distanciaPlayer = 10.0f;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(player.transform.position.x + distanciaPlayer, 2.0f, player.transform.position.z);
     }
+    
 }
